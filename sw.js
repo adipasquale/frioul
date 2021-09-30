79d961b8ba12a37e91f43a1fdbb803a953d9a1b2
Adrien Di Pasquale <adrien@dipasquale.fr>
2021-09-30 18:36:42 +0200

    fix updating service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'frioul-v4';
+const cacheName = 'frioul-16';
 
 const cacheFiles = [
   "./app.js",
@@ -19,13 +19,22 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('fetch', (e) => {
   e.respondWith((async () => {
-    const r = await caches.match(e.request);
+    const cache = await caches.open(cacheName);
+    const r = await cache.match(e.request);
     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
     if (r) { return r; }
     const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
     cache.put(e.request, response.clone());
     return response;
   })());
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }))
+  }));
 });
